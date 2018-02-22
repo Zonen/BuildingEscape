52b52bdcea8badef88d5aa5d267b5be38c70213b
WOLFPAC-LAPTOP\zonen <zonen04@gmail.com>
2018-02-22 10:58:41 -0600

    Using Draw Debug Line

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -2,6 +2,7 @@
 
 #include "Engine/World.h"
 #include "Grabber.h"
+#include "DrawDebugHelpers.h"
 
 #define OUT
 
@@ -43,9 +44,21 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 	// TODO Log out to test
 
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(),
-		*PlayerViewPointRotation.ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(),
+	//	*PlayerViewPointRotation.ToString());
 
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	// Draw red Trace Line
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f);
 	// Ray-cast out to reach distance (private variable)
 
 	// See what we hit
